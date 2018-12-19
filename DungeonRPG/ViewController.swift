7de562a615c2a42a3d6775ff1b74a00705eb10d9
DAM <dam@a22o02.local>
2018-12-19 20:19:02 +0100

    primeros pasos

--- DungeonRPG/ViewController.swift
+++ DungeonRPG/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var magiaAngar: UILabel!
+    
+    @IBOutlet weak var luckyAngar: UILabel!
+    
+    @IBOutlet weak var ataqueAngar: UILabel!
+    
+    @IBOutlet weak var defensaAngar: UILabel!
+    
+    @IBOutlet weak var magiaFelween: UILabel!
+    
+    @IBOutlet weak var luckyFelween: UILabel!
+    
+    @IBOutlet weak var ataqueFelween: UILabel!
+    
+    @IBOutlet weak var defensaFelween: UILabel!
+    
+    @IBOutlet weak var magiaThojen: UILabel!
+   
+    @IBOutlet weak var luckyThojen: UILabel!
+    
+    @IBOutlet weak var ataqueThojen: UILabel!
+    
+    @IBOutlet weak var defensaThojen: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
